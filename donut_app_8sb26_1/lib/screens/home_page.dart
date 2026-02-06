5dfbc1a5e023e3a420967bb44ef5b50fcc3f2e10
Ariadna Leon <ariadna.leon04@gmail.com>
2026-02-06 16:07:58 -0600

    tabBar Added

--- donut_app_8sb26_1/lib/screens/home_page.dart
+++ donut_app_8sb26_1/lib/screens/home_page.dart
@@ -1,4 +1,6 @@
+
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/utils/my_tab.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -8,29 +10,62 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+ List<Widget> myTabs = [
+    //donut tab
+    const MyTab(iconPath: 'lib/icons/donut.png', iconName: 'Donut'),
+    //burger tab
+    const MyTab(iconPath: 'lib/icons/burger.png', iconName: 'Burger'),
+    //smoothie tab
+    const MyTab(iconPath: 'lib/icons/smoothie.png', iconName: 'Smoothie'),
+    //pancake tab
+    const MyTab(iconPath: 'lib/icons/pancakes.png', iconName: 'Pancake'),
+    //pizza tab
+    const MyTab(iconPath: 'lib/icons/pizza.png', iconName: 'Pizza'),
+  ];
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: AppBar(
-      backgroundColor: Colors.transparent,
-      //icono de la izquierda
-      leading: Icon(Icons.menu, color: Colors.grey[800],),
-      //ICONO DE LA DRECHA
-      actions:[
-        Padding(
-          padding: const EdgeInsets.only(right: 24.0),
-          child: Icon(Icons.person, color: Colors.grey[800],),
+    return DefaultTabController(
+      length: myTabs.length,
+      child: Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        // Icono de la izquierda
+        leading: Icon(Icons.menu, color: Colors.grey[800]),
+        // Iconos de la derecha
+        actions: [
+          Padding(
+            padding: const EdgeInsets.only(right: 24.0),
+            child: Icon(Icons.person, color: Colors.grey[800]),
+          ),
+        ],
+      ),
+      body: Column(
+        //1. Texo principal
+        children: [Padding(
+          padding: const EdgeInsets.only(left: 24.0),
+          child: Row(
+            children: [
+              Text('I want to ', style: TextStyle(fontSize: 24)),
+              Text(
+                'Eat',
+               style: TextStyle(
+                //Tamaño de la letra
+                fontSize: 24,
+                //Negritas
+                fontWeight: FontWeight.bold,
+                //Subrayado
+                decoration: TextDecoration.underline,
+                )
+              ),
+            ],
+          ),
         ),
-      ],
+        //2. Pestañas (TabBar)
+         TabBar(tabs: myTabs),
+        ]
+      ),
     ),
-    body: Column(
-      //1. texto principal
-
-      //2. pestañas (TarBar)
-
-      //3. contenmido (TarBarView)
-      
-      //4. carrito (Cart)
-    )
     );
   }
 }
